ea353967287e81eba3888967f354968b90bc5bef
sakamaki <sakamaki@pxgrid.com>
2013-08-27 19:59:11 +0900

    プレビューエリア用のjsをサンプルで追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,51 @@
 module.exports = function(grunt){
-	var path = require('path');
-	var matchde = require('matchdep');
+  var path = require('path');
+  var matchde = require('matchdep');
 
-	// load all grunt-plugin tasks
-	matchde.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // load all grunt-plugin tasks
+  matchde.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-	// init config
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  // init config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     // build stylesheet docs
-		styleguide: {
-			styledocco: {
-				options: {
-					framework: {
-						name: 'styledocco'
-					},
-					name: 'Project Name'
-				},
-				files: {
-					'docs': 'src/scss/**/*.scss'
-				}
-			}
-		},
+    styleguide: {
+      styledocco: {
+        options: {
+          framework: {
+            name: 'styledocco'
+          },
+          name: 'Project Name',
+          // プレビューエリア用のcssやjs(インラインで埋め込まれる)
+          template: {
+            include: ['src/doc-preview.js']
+          }
+        },
+        files: {
+          'docs': 'src/scss/**/*.scss'
+        }
+      }
+    },
     // clean document directory
-		clean: ['docs'],
-		// compile scss to css
-		compassMultiple: {
-			options: {
-				config: 'config.rb',
-				sassDir: 'src/scss/'
-			},
-			common: {}
-		},
-		// watch some files status
-		watch: {
-			app: {
-				files: ['src/scss/**/*.scss'],
-				tasks: ['compassMultiple']
-			}
-		}
-	});
+    clean: ['docs'],
+    // compile scss to css
+    compassMultiple: {
+      options: {
+        config: 'config.rb',
+        sassDir: 'src/scss/'
+      },
+      common: {}
+    },
+    // watch some files status
+    watch: {
+      app: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['compassMultiple']
+      }
+    }
+  });
 
-	// resiter tasks
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['compassMultiple', 'clean', 'styleguide']);
+  // resiter tasks
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['compassMultiple', 'clean', 'styleguide']);
 };
